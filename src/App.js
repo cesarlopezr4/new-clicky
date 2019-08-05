7beb6c72b158152536bc628d88232cf9bd349a30
Cesar "Tito" Lopez <cesar.lopez.rodriguez@gmail.com>
2019-08-05 10:52:57 -0400

    update page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends
+  };
+
+  shuffle = myArray => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    // const friends = this.state.friends.filter(friend => friend.id !== id);
+    // Set this.state.friends equal to the new friends array
+    var i = myArray.length;
+    if ( i === 0 ) return false;
+    while ( --i ) {
+       var j = Math.floor( Math.random() * ( i + 1 ) );
+       var tempi = myArray[i];
+       var tempj = myArray[j];
+       myArray[i] = tempj;
+       myArray[j] = tempi;
+     }
+ 
+    this.setState({ friends: myArray });
+  };
+
+  whenClickFriend = () => {
+    console.log(friends)
+    this.shuffle(friends)
+  }
+
+  
+
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            clickImage={this.whenClickFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+
 }
 
 export default App;
